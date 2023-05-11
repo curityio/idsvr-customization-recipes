a4ff8db57df5bd979844fb3e28c86794ea8d8e30
gary.archer <gary.archer@WORKMAC.local>
2023-05-11 12:49:46 +0200

    Updates

--- deploy-idsvr.sh
+++ deploy-idsvr.sh
@@ -24,20 +24,36 @@ if [ "$RECIPE" != 'basics' -a "$RECIPE" != 'email' -a "$RECIPE" != 'multi-brand'
 fi
 
 #
-# Prepare the recipe, to set file copy paths to apply to docker compose
-# This dot sources a script and sets the CUSTOM_RESOURCES environment variable
+# Check the build volume exists
 #
-. ./recipes/$RECIPE/prepare-deployment.sh
-cd ../../idsvr
+if [ ! -d ./ui-builder/build-vol ]; then
+  echo 'Please run the UI builder before deploying the recipe'
+  exit 1
+fi
+
+#
+# Prepare the recipe, by creating Docker Compose resources
+#
+ROOT='./ui-builder/build-vol'
+rm ./idsvr/custom_resources.txt 2>/dev/null
+cat ./recipes/$RECIPE/files.json | jq -c '.files[]' |
+while IFS=$'\t' read -r c; do
+  FOLDER=$(echo "$c" | jq -r '.folder')
+  FILE=$(echo "$c" | jq -r '.file')
+  echo "     - $ROOT/$FOLDER/$FILE:/opt/idsvr/usr/share/$FOLDER/$FILE" >> ./idsvr/custom_resources.txt
+done
+export CUSTOM_RESOURCES=$(cat ./idsvr/custom_resources.txt)
 
 #
 # Produce the final docker compose file, including customized resources
 #
+cd idsvr
 envsubst < ./docker-compose-template.yaml > ./docker-compose.yaml
 if [ $? -ne 0 ]; then
   echo '*** Problem encountered running envsubst to produce the final docker compose file'
   exit 1
 fi
+rm custom_resources.txt
 
 #
 # Run the deployment
